1783741defff8e50046cd3a06887369b61c57265
Henrique <henriquesg09@hotmail.com>
2021-08-23 00:05:49 -0300

    feat(base): add more global variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,11 @@
-export const API_URL = process.env.REACT_APP_API_URL as string;
+export const APP_NAME = 'React Leap 🚀👨‍🚀';
 
 export const BRAND_COLOR = '#135f9b';
+
+export const API_URL = process.env.REACT_APP_API_URL as string;
+
+export const ENVIROMENT = process.env.NODE_ENV;
+
+export const DEFAULT_PUBLIC_PAGE = '/entrar';
+
+export const DEFAULT_PRIVATE_PAGE = '/';
