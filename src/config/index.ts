f003fb9ddb5a5b294d57e1587f2b9798ced698ce
Henrique <henriquesg09@hotmail.com>
2021-09-05 18:13:09 -0300

    feat(config): add jwt global variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,7 @@ export const APP_NAME = 'React Leap 🚀👨‍🚀';
 export const BRAND_COLOR = '#135f9b';
 
 export const API_URL = process.env.REACT_APP_API_URL as string;
+export const MOCK_API_URL = process.env.REACT_APP_MOCK_API_URL as string;
 
 export const ENVIROMENT = process.env.NODE_ENV;
 
@@ -16,4 +17,7 @@ export const USER_INFO_ID = '_react-leap_user-info';
 
 export const MSW_DB = '_react-leap_msw-db';
 
-export const JWT_SECRET = '123456' as string;
+export const JWT_ACCESS_SECRET = '123456' as string;
+export const JWT_ACCESS_EXPIRES_IN = 10;
+export const JWT_REFRESH_SECRET = '123456789' as string;
+export const JWT_REFRESH_EXPIRES_IN = 50;
