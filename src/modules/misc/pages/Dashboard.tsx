49d9f33f2eeaa920f3e3cf720d198994161fbe92
Henrique <henriquesg09@hotmail.com>
2021-09-05 17:50:53 -0300

    feat(dashboard): add dashboard

--- src/modules/misc/pages/Dashboard.tsx
+++ src/modules/misc/pages/Dashboard.tsx
@@ -1,4 +1,5 @@
-// import { ContentLayout } from '@/components/Layout';
+import { Box } from '@chakra-ui/react';
+
 import { Page } from '@/components/Page';
 import { useAuth } from '@/lib/authentication';
 import { ROLES } from '@/lib/authorization/permissions/roles';
@@ -6,29 +7,31 @@ import { ROLES } from '@/lib/authorization/permissions/roles';
 export const Dashboard = () => {
   const { user } = useAuth();
   return (
-    <Page>
-      <h1 className="text-xl mt-2">
-        Welcome <b>{`${user?.firstName} ${user?.lastName}`}</b>
-      </h1>
-      <h4 className="my-3">
-        Your role is : <b>{user?.role}</b>
-      </h4>
-      <p className="font-medium">In this application you can:</p>
-      {user?.role === ROLES.USER && (
-        <ul className="my-4 list-inside list-disc">
-          <li>Create comments in discussions</li>
-          <li>Delete own comments</li>
-        </ul>
-      )}
-      {user?.role === ROLES.ADMIN && (
-        <ul className="my-4 list-inside list-disc">
-          <li>Create discussions</li>
-          <li>Edit discussions</li>
-          <li>Delete discussions</li>
-          <li>Comment on discussions</li>
-          <li>Delete all comments</li>
-        </ul>
-      )}
+    <Page title="Dashboard">
+      <Box minH="200vh">
+        <h1 className="text-xl mt-2">
+          Welcome <b>{`${user?.firstName} ${user?.lastName}`}</b>
+        </h1>
+        <h4 className="my-3">
+          Your role is : <b>{user?.role}</b>
+        </h4>
+        <p className="font-medium">In this application you can:</p>
+        {/* {user?.role === ROLES.USER && (
+          <ul className="my-4 list-inside list-disc">
+            <li>Create comments in discussions</li>
+            <li>Delete own comments</li>
+          </ul>
+        )} */}
+        {user?.role === ROLES.ADMIN && (
+          <ul className="my-4 list-inside list-disc">
+            <li>Create discussions</li>
+            <li>Edit discussions</li>
+            <li>Delete discussions</li>
+            <li>Comment on discussions</li>
+            <li>Delete all comments</li>
+          </ul>
+        )}
+      </Box>
     </Page>
   );
 };
