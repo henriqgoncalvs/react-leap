24eb7163de0290cf021b5027bb4feb81dfe47669
Henrique <henriquesg09@hotmail.com>
2021-09-06 15:38:05 -0300

    fix(styles): fix textarea style

--- src/styles/theme/components/textarea.ts
+++ src/styles/theme/components/textarea.ts
@@ -3,10 +3,8 @@ import { mode } from '@chakra-ui/theme-tools';
 export default {
   variants: {
     outline: (props) => ({
-      field: {
-        bg: mode('blackAlpha.50', 'whiteAlpha.50')(props),
-        borderColor: mode('blackAlpha.100', 'whiteAlpha.100')(props),
-      },
+      bg: mode('blackAlpha.50', 'whiteAlpha.50')(props),
+      borderColor: mode('blackAlpha.100', 'whiteAlpha.100')(props),
     }),
   },
   defaultProps: {
