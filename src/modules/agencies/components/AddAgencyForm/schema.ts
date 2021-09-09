8756f276933b98ffece6c3c7cd91753e9cbc3742
Henrique <henriquesg09@hotmail.com>
2021-09-09 17:36:00 -0300

    feat(agencies): remove agencies module

--- src/modules/agencies/components/AddAgencyForm/schema.ts
+++
@@ -1,8 +0,0 @@
-import { v } from '@/lib/validator';
-
-export default v.object().shape({
-  name: v.string().required('Name is required'),
-  bio: v.string().required('Bio is required'),
-  phone: v.string().required('Phone is required'),
-  email: v.string().email('Email is required').required('Email is required'),
-});
