02f52a03e4bc8a9c5ec323c766397af6c3390c72
Henrique <henriquesg09@hotmail.com>
2021-09-09 18:14:02 -0300

    fix(routes): remove unused routes and add income and expense

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -2,8 +2,10 @@ import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 
 import { MainLayout } from '@/components/Layout';
 // import { DiscussionsRoutes } from '@/modules/discussions';
-import { AgenciesRoutes } from '@/modules/agencies';
-import { Landing, Dashboard, Users } from '@/modules/misc';
+import { Dashboard } from '@/modules/dashboard';
+import { ExpensesRoutes } from '@/modules/expenses';
+import { IncomesRoutes } from '@/modules/incomes';
+import { Landing, Users, About } from '@/modules/misc';
 // import { Profile, Users } from '@/modules/users';
 
 const App = () => {
@@ -19,7 +21,9 @@ export const ProtectedRoutes = () => {
     <Routes>
       <Route path="/app" element={<App />}>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/agencies/*" element={<AgenciesRoutes />} />
+        <Route path="/about/*" element={<About />} />
+        <Route path="/expenses/*" element={<ExpensesRoutes />} />
+        <Route path="/incomes/*" element={<IncomesRoutes />} />
         <Route path="/users/*" element={<Users />} />
         {/* <Route path="/profile" element={<Profile />} /> */}
         <Route path="*" element={<Navigate to="/app" />} />
