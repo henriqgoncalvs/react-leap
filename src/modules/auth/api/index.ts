54ae06c2db776f29ceb58f143874cef01dfcf140
Henrique <henriquesg09@hotmail.com>
2022-01-12 18:31:35 -0300

    build: removed unused libraries

--- src/modules/auth/api/index.ts
+++ src/modules/auth/api/index.ts
@@ -3,13 +3,13 @@ import { LoginCredentials, UserResponse, RegisterCredentials, AuthUser } from '.
 import axios from '@/lib/axios';
 
 export const loginWithEmailAndPassword = (data: LoginCredentials): Promise<UserResponse> => {
-  return axios.unauthorized({ mock: true }).post('/auth/login', data);
+  return axios.unauthorized().post('/auth/login', data);
 };
 
 export const registerWithEmailAndPassword = (data: RegisterCredentials): Promise<UserResponse> => {
-  return axios.unauthorized({ mock: true }).post('/auth/register', data);
+  return axios.unauthorized().post('/auth/register', data);
 };
 
 export const getUserProfile = (): Promise<AuthUser> => {
-  return axios.authorized({ mock: true }).get('/auth/me');
+  return axios.authorized().get('/auth/me');
 };
--- src/modules/incomes/api/index.ts
+++ src/modules/incomes/api/index.ts
@@ -10,21 +10,21 @@ import {
 import axios from '@/lib/axios';
 
 export const getIncomes = (): Promise<Income[]> => {
-  return axios.authorized({ mock: true }).get(`/income`);
+  return axios.authorized().get(`/income`);
 };
 
 export const getIncome = ({ id }: GetIncomeOptions): Promise<Income> => {
-  return axios.authorized({ mock: true }).get(`/income/${id}`);
+  return axios.authorized().get(`/income/${id}`);
 };
 
 export const createIncome = ({ data }: CreateIncomeOptions): Promise<Income> => {
-  return axios.authorized({ mock: true }).post(`/income`, data);
+  return axios.authorized().post(`/income`, data);
 };
 
 export const updateIncome = ({ id, data }: UpdateIncomeOptions): Promise<Income> => {
-  return axios.authorized({}).patch(`/income/${id}`, data);
+  return axios.authorized().patch(`/income/${id}`, data);
 };
 
 export const deleteIncome = ({ id }: DeleteIncomeOptions) => {
-  return axios.authorized({}).patch(`/income/${id}`);
+  return axios.authorized().patch(`/income/${id}`);
 };
