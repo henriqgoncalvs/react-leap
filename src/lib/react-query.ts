8dce2ab22a9cb120160cc97c5bc2598159f44047
Viniciuslb067 <viniciuslb067@gmail.com>
2022-04-04 14:58:03 -0300

    feat(auth-context): fix query config type

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,6 +1,5 @@
 import { AxiosError } from 'axios';
 import { QueryClient, UseQueryOptions, UseMutationOptions, DefaultOptions } from 'react-query';
-import { PromiseValue } from 'type-fest';
 
 const queryConfig: DefaultOptions = {
   queries: {
@@ -13,11 +12,11 @@ const queryConfig: DefaultOptions = {
 export const queryClient = new QueryClient({ defaultOptions: queryConfig });
 
 export type QueryConfig<FetcherFnType extends (...args: any) => any> = UseQueryOptions<
-  PromiseValue<ReturnType<FetcherFnType>>
+  Awaited<ReturnType<FetcherFnType>>
 >;
 
 export type MutationConfig<FetcherFnType extends (...args: any) => any> = UseMutationOptions<
-  PromiseValue<ReturnType<FetcherFnType>>,
+  Awaited<ReturnType<FetcherFnType>>,
   AxiosError,
   Parameters<FetcherFnType>[0]
 >;
