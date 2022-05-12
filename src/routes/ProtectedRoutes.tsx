7f66b40c69bcaa46fcb003fa5e3fd0189e1dcd8f
Edimario Junior <edimariojunior.14@gmail.com>
2022-05-11 23:20:37 -0300

    fix: fix routes and msw

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -1,7 +1,6 @@
 import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 
 import { MainLayout } from '@/components/Layout';
-import { Dashboard } from '@/modules/dashboard';
 import { ExpensesRoutes } from '@/modules/expenses';
 import { IncomesRoutes } from '@/modules/incomes';
 import { Landing, Users, About } from '@/modules/misc';
@@ -18,11 +17,11 @@ export const ProtectedRoutes = () => {
   return (
     <Routes>
       <Route path="/app" element={<App />}>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/about/*" element={<About />} />
-        <Route path="/expenses/*" element={<ExpensesRoutes />} />
-        <Route path="/incomes/*" element={<IncomesRoutes />} />
-        <Route path="/users/*" element={<Users />} />
+        <Route path="" element={<About />} />
+        <Route path="about/*" element={<About />} />
+        <Route path="expenses/*" element={<ExpensesRoutes />} />
+        <Route path="incomes/*" element={<IncomesRoutes />} />
+        <Route path="users/*" element={<Users />} />
         <Route path="*" element={<Navigate to="/app" />} />
       </Route>
       <Route path="/" element={<Landing />} />
