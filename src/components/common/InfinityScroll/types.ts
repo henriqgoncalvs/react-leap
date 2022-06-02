82050b1fc25a9f6c5cba8ba86de7c2b869812ecc
Alexandre Henrique Soares <ahssf01@gmail.com>
2022-06-02 14:30:56 -0300

    feature: create hook useInfinity scroll

--- src/components/common/InfinityScroll/types.ts
+++ src/components/common/InfinityScroll/types.ts
@@ -1,8 +1,12 @@
 export type UseInfinityScrollProps = {
   queryFn: (props: any) => Promise<any>;
   take: number;
+  filters: Record<string, any>;
+  queryKey: string;
 };
 
 export type UseInfinityScrollReturn<T = unknown> = {
   data: T[];
+  onEndReached: () => void;
+  isLoading: boolean;
 };
--- src/components/common/InfinityScroll/useInfinityScroll.tsx
+++ src/components/common/InfinityScroll/useInfinityScroll.tsx
@@ -4,18 +4,28 @@ import { useQuery } from 'react-query';
 import { UseInfinityScrollProps, UseInfinityScrollReturn } from './types';
 export const useInfinityScroll = <T,>({
   queryFn,
+  filters,
+  take,
+  queryKey,
 }: UseInfinityScrollProps): UseInfinityScrollReturn<T> => {
   const [page, setPage] = useState(0);
   const [data, setData] = useState<T[]>([]);
-  const { data: queryData } = useQuery<T[]>({ queryFn });
+  const { data: queryData, isLoading } = useQuery<{ data: T[]; totalItems: number }>(
+    [queryKey, filters, page],
+    () => queryFn({ ...filters, take, skip: take * page }),
+  );
 
   useEffect(() => {
-    if (queryData) {
-      if (queryData.length) setData((prev) => [...prev, ...queryData]);
+    if (queryData && queryData.data && queryData.data.length) {
+      setData((prev) => [...prev, ...queryData.data]);
     }
   }, [queryData]);
 
-  const onEndReached = useCallback(() => {}, []);
+  const onEndReached = useCallback(() => {
+    if (queryData?.data && queryData?.data.length >= take) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  }, [queryData, take]);
 
-  return { data };
+  return { data, onEndReached, isLoading };
 };
