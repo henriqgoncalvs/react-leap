3ca3946ef170aff217550d2ae8019d745ed1fb7e
Igor Phillipe <IgrPhillipe@gmail.com>
2022-09-01 09:25:15 -0300

    Update TextInputMask.tsx

--- src/components/Form/Fields/TextInputMask/TextInputMask.tsx
+++ src/components/Form/Fields/TextInputMask/TextInputMask.tsx
@@ -33,39 +33,17 @@ export const TextInputMask = ({
   const { name, value } = field;
   const { touched } = meta;
   const { isValidating, isSubmitting, setFieldValue } = form;
-  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <InputGroup size={size}>
       <Input
         {...field}
         as={InputMask}
-        type={showPassword ? 'text' : type || 'text'}
         placeholder={placeholder ? String(placeholder) : ''}
         value={value}
         onChange={onChange || ((e) => setFieldValue(name, e.target.value))}
         {...restProps}
       />
-
-      {type === 'password' && (
-        <InputLeftElement>
-          <IconButton
-            onClick={() => setShowPassword((x) => !x)}
-            aria-label={showPassword ? 'Hide password' : 'Show password'}
-            display="flex"
-            size="xs"
-            fontSize="lg"
-            icon={showPassword ? <RiEyeLine /> : <RiEyeCloseLine />}
-            variant="unstyled"
-          />
-        </InputLeftElement>
-      )}
-
-      {(touched || isSubmitting) && isValidating && (
-        <InputRightElement>
-          <Spinner />
-        </InputRightElement>
-      )}
     </InputGroup>
   );
 };
